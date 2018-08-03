4f2db15d2d604b2ad3f76d0c5277a7d8c85f43a2
GOOD <succeswilson@gmail.com>
2018-08-03 10:48:42 +0100

    Factorial code

--- Factorial.c
+++ Factorial.c
@@ -1,5 +1,10 @@
+/////////////////////////////////////////////////////////////////////
+ **********PROGRAM TO COMPUTE FACTORIAL USING RECURSION*****
+////////////////////////////////////////////////////////////////////
+
 #include <stdio.h>
 
+//Factorial function
 int Fact( int n ) {
  if ( n == 0 ){
   return 0;
@@ -10,9 +15,14 @@ int Fact( int n ) {
    else {
    return n * Fact( n-1 );
    }
-
-void main( ) {
-int ans = Fact(4);
-printf("The Answer is %d",ans );
+// main function
+int main( void ) {
+// number input declaration
+int num = 0;
+//prompt user for number input
+printf("Enter a Number and I will Compute it Factorial: ");
+scanf("%d",&num);
+int ans = Fact(num);
+printf("Factorial of is %d",ans );
 return 0;
 }
